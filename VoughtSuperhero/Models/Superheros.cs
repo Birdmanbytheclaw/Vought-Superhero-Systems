caac2dc7a2749ef987678a674ea9161772385b8d
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-02-24 15:44:30 -0600

    Working on Models

--- VoughtSuperhero/Models/Superheros.cs
+++ VoughtSuperhero/Models/Superheros.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace VoughtSuperhero.Models
 {
     public class Superheros
     {
+        public int ID { get; set; }
+        [Required]
+
+        public string HeroName { get; set; }
+        [Required]
+        public string AlterEgo { get; set; }
+        [Required]
+        public string SuperPowerOne { get; set; }
+        [Required]
+        public string SuperPowerTwo { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string Catchphrase { get; set; }
     }
 }
